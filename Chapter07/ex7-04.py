00dc42fa959b05b15886e1b3de4a88d08bcd3269
ITC-c24021 <c24021@std.it-college.ac.jp>
2025-05-29 15:02:44 +0900

    P104-P115 第7章演習問題終了

---
+++ Chapter07/ex7-04.py
@@ -0,0 +1,12 @@
+people = {"G.Bluth ll":"A.Development",
+          
+          "Barney":"HIMYM",
+          
+          "Dennis":"Always Sunny",
+
+          }
+
+
+for character in people:
+
+    print(character)
---
+++ Chapter07/ex7-14.py
@@ -0,0 +1,13 @@
+i = 1
+
+while i <= 5:
+
+    if i == 3:
+
+        i += 1
+
+        continue
+
+    print(i)
+
+    i += 1
